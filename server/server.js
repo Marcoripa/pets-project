6885da86093801eef2552aad87129538acdb6d7d
Marco Ripa <marcoripa@hotmail.it>
2022-08-08 15:45:33 +0200

    Connect routes to the server

--- server/server.js
+++ server/server.js
@@ -1,2 +1,15 @@
 const express = require('express')
 const app = express()
+
+//Import the Router
+const tasks = require('./routes/Tasks')
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('Homepage')
+})
+
+app.use('/tasks', tasks)
+
+const port = 4000
+app.listen(port, console.log(`Listening on port: ${port}`))
