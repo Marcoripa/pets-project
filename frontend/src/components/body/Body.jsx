e7c9754a5b29c673093b2eefc8d3120acac872bd
Marco Ripa <marcoripa@hotmail.it>
2022-08-09 18:19:13 +0200

    Folder re-organization

--- frontend/src/components/body/Body.jsx
+++ frontend/src/components/body/Body.jsx
@@ -1,21 +1,26 @@
 import React, { useState, useEffect } from "react";
 import Card from "../card/Card";
 import { BiSearch } from "react-icons/bi";
+// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 export default function Body() {
   const [data, setData] = useState([]);
-  const [location, setLocation] = useState("")
-  const [address, setAddress] = useState("")
+  const [location, setLocation] = useState("");
+  const [address, setAddress] = useState("");
 
   function collectData(e) {
     e.preventDefault();
     fetch("http://localhost:4000/tasks")
       .then((response) => response.json())
-      .then((data) => setData(data.filter(item => 
-        item.Location === location || item.Address === address)
-    ))  
-    setLocation("")
-    setAddress("")
+      .then((data) =>
+        setData(
+          data.filter(
+            (item) => item.Location === location || item.Address === address
+          )
+        )
+      );
+    setLocation("");
+    setAddress("");
   }
 
   return (
@@ -40,11 +45,24 @@ export default function Body() {
           <BiSearch id="button-icon" />
         </button>
       </form>
-      {data == null ? <h1>What are you up to today?</h1> : data.map(item => {
-        return (
-            <Card item={item}/>
-        )
-      })}
+      {data == null ? (
+        <h1>What are you up to today?</h1>
+      ) : (
+        data.map((item) => {
+          return <Card item={item} />;
+        })
+      )}
+      {/* <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer> */}
     </main>
   );
 }
